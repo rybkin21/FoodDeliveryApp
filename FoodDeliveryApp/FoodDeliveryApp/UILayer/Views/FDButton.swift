0a0bc1b227bc6b8e73499b8fcf3a4a8e920753ba
ivanrybkin <rybkin2517@mail.ru>
2024-07-09 13:33:34 +0300

    Added custom FDButton view

---
+++ FoodDeliveryApp/FoodDeliveryApp/UILayer/Views/FDButton.swift
@@ -0,0 +1,78 @@
+//
+//  FDButton.swift
+//  FoodDeliveryApp
+//
+//  Created by Ivan Rybkin on 09.07.2024.
+//
+
+import UIKit
+
+enum FDButtonColorSchemes {
+    case white
+    case orange
+    case grey
+}
+
+class FDButton: UIView {
+
+    private let button = UIButton()
+    var action: (() -> Void)?
+    var scheme: FDButtonColorSchemes = .white {
+        didSet {
+            setColorScheme(scheme: scheme)
+        }
+    }
+
+    init(scheme: FDButtonColorSchemes = .white) {
+        super.init(frame: .zero)
+        setupLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupLayout() {
+        self.backgroundColor = .clear
+        setupButton()
+    }
+
+    private func setupButton() {
+        addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.titleLabel?.font = .Roboto.bold.size(of: 18)
+        button.layer.cornerRadius = 24
+        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+
+        NSLayoutConstraint.activate([
+            button.bottomAnchor.constraint(equalTo: self.bottomAnchor),
+            button.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            button.topAnchor.constraint(equalTo: self.topAnchor)
+        ])
+        setColorScheme(scheme: .white)
+    }
+
+    @objc private func buttonPressed() {
+        guard let action = self.action else { return }
+        action()
+    }
+
+    private func setColorScheme(scheme: FDButtonColorSchemes) {
+        switch scheme {
+        case .white:
+            button.backgroundColor = AppColors.white
+            button.setTitleColor(AppColors.accentOrange, for: .normal)
+        case .orange:
+            button.backgroundColor = AppColors.accentOrange
+            button.setTitleColor(AppColors.white, for: .normal)
+        case .grey:
+            button.backgroundColor = AppColors.grey
+            button.setTitleColor(AppColors.black, for: .normal)
+        }
+    }
+
+    public func setTitle(_ title: String?) {
+        button.setTitle(title, for: .normal)
+    }
+}
